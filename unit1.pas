2404d41e7b06355b1ab7b59026d7684d7a3b6bfb
kerabatsos <34196230+kerabatsos@users.noreply.github.com>
2017-12-09 20:50:20 +0100

    spojazdnenie loginu
    heslo je 1234

--- unit1.pas
+++ unit1.pas
@@ -7,7 +7,6 @@ interface
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
   LCLType, Buttons, ShellApi, Types;
-
 type
 
   { TForm1 }
@@ -22,6 +21,7 @@ type
     Button7: TButton;
     Edit2: TEdit;
     Label1: TLabel;
+    Label2: TLabel;
     ListBox1: TListBox;
     ListBox2: TListBox;
     Memo1: TMemo;
@@ -36,9 +36,18 @@ type
     { public declarations }
   end;
 
+  Loginy = record
+    meno: string;
+    heslo: integer;
+  end;
+
 var
   Form1: TForm1;
   itemIndex: integer;
+  medzisucet: integer;
+  prihlasenie: textfile;
+  menoPokladnika: string;
+  menoHeslo: array[1..50] of Loginy;
 
 implementation
 
@@ -60,7 +69,11 @@ end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+
+
+
    Login;
+   Label2.Caption := menoPokladnika;
    ListBox1.Items.Clear;             //Delete all existing strings
   ListBox1.Items.Add('First line');
   ListBox1.Items.Add('Second Line');
@@ -93,21 +106,45 @@ end;
 
 procedure TForm1.Login;
 var
-  password: string;
+  passwordIn: string;
   q: boolean;
+  size, i ,j : integer;
+  udaj: string;
 begin
+  AssignFile( prihlasenie , 'pokladnici.txt' );
+  Reset( prihlasenie );
+  Read( prihlasenie , size );
+  ReadLn( prihlasenie , udaj );
+
+  for i := 1 to size do
+     begin
+        ReadLn( prihlasenie , udaj );
+        menoHeslo[i].meno := udaj;
+
+        ReadLn( prihlasenie , udaj );
+        menoHeslo[i].heslo := strtoint( udaj );
+     end;
+
+
   q := false;
 
-  InputQuery( 'LOGIN' , 'Zadajte heslo:' , True , password );
+  InputQuery( 'LOGIN' , 'Zadajte heslo:' , True , passwordIn );
 
   repeat
-  if password = '1' then
-     q := True
-  else
-   begin
-    ShowMessage( 'fok of' );
-    InputQuery( 'LOGIN' , 'Zadajte heslo:' , True , password );
-   end;
+  for j := 1 to size do
+     begin
+          if strtoint( passwordIn ) = menoHeslo[ j ].heslo then
+            begin
+             q := True;
+             menoPokladnika := menoHeslo[ j ].meno;
+             Break;
+            end;
+      end;
+          if q = false then
+            begin
+             ShowMessage( 'Nesprávne heslo' );
+             InputQuery( 'LOGIN' , 'Zadajte heslo:' , True , passwordIn );
+            end;
   until q = True;
 end;
 
